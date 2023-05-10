84795339256837c177b370d4c6f1ae34b2e910cd
Jacek Dobrowolski <293354@pw.edu.pl>
2023-05-10 23:59:25 +0200

    some test

---
+++ test.py
@@ -0,0 +1,57 @@
+
+# %%
+from itertools import repeat
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+plt.rcParams['figure.figsize'] = [8, 8]
+
+# %%
+cap = cv.VideoCapture("data/0001-0030.avi")
+
+# %%
+ret, first_frame = cap.read()
+prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+mask = np.zeros_like(first_frame)
+mask[..., 1] = 255
+# plt.imshow(prev_gray, cmap='gray')
+
+# %%
+
+def dense_flow(frames: int):
+    cap = cv.VideoCapture("data/0001-0030.avi")
+    ret, first_frame = cap.read()
+    prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+    flow = None
+    for _ in range(frames):
+        ret, frame = cap.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        flow = cv.calcOpticalFlowFarneback(
+            prev=prev_gray,
+            next=gray, 
+            flow=flow,
+            pyr_scale=0.5,
+            levels=5,
+            winsize=5,
+            iterations=3,
+            poly_n=5,
+            poly_sigma=1.2,
+            flags=0
+        )
+        prev_gray = gray
+
+    magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
+    mask[..., 0] = angle * 180 / np.pi / 2
+    mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+    rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
+    # return rgb
+    return magnitude
+
+# %%
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+flow = dense_flow(3)
+ys, xs = flow.shape
+Y, X = np.mgrid[0:ys, 0:xs]
+ax.plot_surface(X, Y, flow, cmap='plasma')
+plt.show()
